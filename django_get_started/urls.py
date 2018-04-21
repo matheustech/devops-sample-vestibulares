38821fc9cfb43c3ff022e46af6ce055b8369bfce
Guilherme Miotto <36551693+Guivmiotto@users.noreply.github.com>
2018-04-21 15:39:41 -0300

    Update urls.py

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -6,13 +6,13 @@ from datetime import datetime
 from django.conf.urls import patterns, url
 from app.forms import BootstrapAuthenticationForm
 
-# Uncomment the next lines to enable the admin:
-# from django.conf.urls import include
-# from django.contrib import admin
-# admin.autodiscover()
+#Uncomment the next lines to enable the admin:
+#from django.conf.urls import include
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
+    #Examples:
     url(r'^$', 'app.views.home', name='home'),
     url(r'^contact$', 'app.views.contact', name='contact'),
     url(r'^about', 'app.views.about', name='about'),
@@ -36,9 +36,9 @@ urlpatterns = patterns('',
         },
         name='logout'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #Uncomment the admin/doc line below to enable admin documentation:
+    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    #Uncomment the next line to enable the admin:
+    #url(r'^admin/', include(admin.site.urls)),
 )
